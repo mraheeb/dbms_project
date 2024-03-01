18cfebad16d868a4cfddfb2bf089c8a4b9e39618
mraheeb <mraheeb34@gmail.com>
2024-03-01 12:29:56 +0530

    fixed: loading and updation of values in projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -6,11 +6,8 @@ router.route('/')
     // GET request to retrieve all projects
     .get(async (req, res) => {
         try {
-
             const connection = await getConnection();
             const [rows] = await connection.execute('SELECT * FROM projects');
-            // console.log('Fetched projects:', rows); // Log fetched projects
-            // await connection.end();
             res.status(200).json(rows);
         } catch (error) {
             console.error('Error retrieving projects:', error);
@@ -23,20 +20,21 @@ router.route('/')
             const { project_name, project_description, start_date, end_date, status } = req.body;
             const connection = await getConnection();
             await connection.execute('INSERT INTO projects (project_name, project_description, start_date, end_date, status) VALUES (?, ?, ?, ?, ?)', [project_name, project_description, start_date, end_date, status]);
-            // await connection.end();
             res.status(201).json({ message: 'Project created successfully' });
         } catch (error) {
             console.error('Error creating project:', error);
             res.status(500).json({ error: 'Failed to create project' });
         }
-    })
+    });
     // PUT request to update project status
-    .put(async (req, res) => {
+    router.put('/:project_id',async (req, res) => {
         try {
-            const { project_id, status } = req.body;
+            const { project_id } = req.params; // Extract project_id from URL params
+            
+            const { status } = req.body; // Extract status from request body
+            // console.log("within projects.js value of projectid and status",project_id,status);
             const connection = await getConnection();
             await connection.execute('UPDATE projects SET status = ? WHERE project_id = ?', [status, project_id]);
-            // await connection.end(); 
             res.status(200).json({ message: 'Project status updated successfully' });
         } catch (error) {
             console.error('Error updating project status:', error);
